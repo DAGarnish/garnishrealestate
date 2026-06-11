603a519df4835b352b27b2b84ae4018a8728d26b
abhises <abhisespoudyal@gmail.com>
2026-06-11 12:38:16 +0545

    remove everything

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 export default function Home() {
   return (
-    <main className="min-h-screen bg-white flex items-center justify-center">
-      <h1 className="text-black text-2xl font-sans">hello world</h1>
+    <main className="min-h-screen bg-white">
     </main>
   );
 }
